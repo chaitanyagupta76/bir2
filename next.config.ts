cc7570eabfb0067beda8554e81f1efaa7dc2168d
chaitanya <chaitanyagupta.gamini@gmail.com>
2026-05-03 20:53:54 +0530

    dsfsdfds

--- next.config.ts
+++ next.config.ts
@@ -5,19 +5,8 @@ const nextConfig: NextConfig = {
   images: { unoptimized: true },
   /* config options here */
   reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
+
 
